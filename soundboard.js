0aaf7740c3a26687e0f4b2e181defe00c2a890c4
jlago <lagorege@msu.edu>
2021-11-18 20:22:11 -0500

    stuff

--- soundboard.js
+++ soundboard.js
@@ -1,22 +1,26 @@
-var button1 = document.getElementById('first');
-var button2 = document.getElementById('second');
-var button3 = document.getElementById('third');
+var button1 = document.getElementById('first')
+var button2 = document.getElementById('second')
+var button3 = document.getElementById('third')
 
-button1.addEventListener('mouseenter', playBell);
-button2.addEventListener('mouseenter', playRegister);
-button3.addEventListener('mouseenter', playCymbals);
+button1.addEventListener('mouseenter', playBell)
+button2.addEventListener('mouseenter', playRegister)
+button3.addEventListener('mouseenter', playCymbals)
+
+button1.addEventListener('click', playBell)
+button2.addEventListener('click', playRegister)
+button3.addEventListener('click', playCymbals)
 
 function playBell() {
-    var audio_bell = new Audio("boxing_bell.wav");
-    audio_bell.play();
+    var audio_bell = new Audio("boxing_bell.wav")
+    audio_bell.play()
 }
 
 function playRegister() {
-    var audio_reg = new Audio("cash_register_x.wav");
-    audio_reg.play();
+    var audio_reg = new Audio("cash_register_x.wav")
+    audio_reg.play()
 }
 
 function playCymbals() {
-    var audio_cym = new Audio("cymbals.wav");
-    audio_cym.play();
+    var audio_cym = new Audio("cymbals.wav")
+    audio_cym.play()
 }
